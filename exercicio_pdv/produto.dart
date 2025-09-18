1b4daa968a52422e2eb00c76704b5c220a2ad7cb
João Paulo Santana <144402881+joaossantanadev@users.noreply.github.com>
2025-09-18 00:40:13 +0000

    feat:compras

---
+++ exercicio_pdv/produto.dart
@@ -0,0 +1,17 @@
+void main(){
+
+
+}
+
+class Produto{
+    String nome;
+    double valorUnitario;
+
+    Produto(this.nome, this.valorUnitario);
+
+
+    @override
+    String toString(){
+        return 'Produto: $nome - Valor: R\${valorUnitario.toStringAsFixed(2)}';
+    }
+}
